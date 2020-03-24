b3093fe7a88343c6da103c75ff76b26d6f0d7699
SimonHauck <simonhauck@gmx.de>
2020-03-24 16:58:14 +0100

    test for codecov badge

--- code/tests/test_second.py
+++ code/tests/test_second.py
@@ -1,10 +1,15 @@
 import unittest
 import helper as h
+from test_class import TestClass
 
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
         self.assertEqual(h.subtract(3, 2), 1)
+
+    def test_testclass(self):
+        testclass = TestClass()
+        self.assertEqual(testclass.add_class(3, 5), 8)
 
 
 if __name__ == '__main__':
