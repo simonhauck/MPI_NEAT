9a21b774f1b3c474e27361335126fdabffd5f03f
Simon Hauck <simonhauck@gmx.de>
2020-05-13 16:18:28 -0700

    implement and test innovation number generator for single core

--- code/src/neat_single_core/inno_number_generator_single_core.py
+++ code/src/neat_single_core/inno_number_generator_single_core.py
@@ -14,14 +14,27 @@ class InnovationNumberGeneratorSingleCore(InnovationNumberGeneratorInterface):
         self.connection_innovations = []
 
     def next_generation(self, generation: int) -> None:
-        super().next_generation(generation)
+        """
+        Indicate that a new generation is evaluated. This resets the stored node innovations
+        :param generation: not used
+        :return: None
+        """
+        self.node_innovations = []
 
     def get_node_innovation_number(self, node1: Node = None, node2: Node = None) -> Union[int, str]:
+        """
+        Get a node innovation number. If the node is generated through mutation, the given node1 and node2, indicate
+        between which nodes the node is generated (values from the split connection). If one of the given nodes is None,
+        it will generate a new number
+        :param node1: the in node of the splitted connection. Can be none for initial nodes
+        :param node2: the out node of the splitted connectino. Can be none for initial nodes
+        :return: the generated innovation number
+        """
         if node1 is None or node2 is None:
             return self._get_new_node_number()
 
         for n1_innovation, n2_innovation, innovation in self.node_innovations:
-            if n1_innovation == node1.innovation_number and n2_innovation == n2_innovation:
+            if n1_innovation == node1.innovation_number and n2_innovation == node2.innovation_number:
                 return innovation
 
         # No stored record found, create a new one
@@ -31,6 +44,13 @@ class InnovationNumberGeneratorSingleCore(InnovationNumberGeneratorInterface):
         return new_number
 
     def get_connection_innovation_number(self, input_node: Node = None, output_node: Node = None) -> Union[int, str]:
+        """
+        Get a connection innovation number. If the connection is generated through mutation, the given input and output-
+        node should be given for the node
+        :param input_node: the input node of the connection. Can be none for initial connections
+        :param output_node: the output node of the connection. Can be none for initial connections
+        :return: the generated innovation number
+        """
         if input_node is None or output_node is None:
             return self._get_new_connection_number()
 
--- code/tests/neat_single_core/test_inno_number_generator_single_core.py
+++ code/tests/neat_single_core/test_inno_number_generator_single_core.py
@@ -1,5 +1,7 @@
 from unittest import TestCase
 
+from neat_core.activation_function import step_function
+from neat_core.models.node import Node, NodeType
 from neat_single_core.inno_number_generator_single_core import InnovationNumberGeneratorSingleCore
 
 
@@ -8,13 +10,65 @@ class InnovationNumberGeneratorSingleCoreTest(TestCase):
     def setUp(self) -> None:
         self.generator = InnovationNumberGeneratorSingleCore()
 
-    def test(self):
-        self.assertEqual(0, self.generator.get_connection_innovation_number())
-        self.assertEqual(1, self.generator.get_connection_innovation_number())
-        self.assertEqual(2, self.generator.get_connection_innovation_number())
-        self.assertEqual(3, self.generator.get_connection_innovation_number())
+    def test_next_generation(self):
+        node1 = Node(1, NodeType.INPUT, step_function, 0)
+        node2 = Node("abc", NodeType.HIDDEN, step_function, 0.5)
+        node3 = Node(3, NodeType.OUTPUT, step_function, 1)
 
-        self.assertEqual(0, self.generator.get_node_innovation_number())
-        self.assertEqual(1, self.generator.get_node_innovation_number())
-        self.assertEqual(2, self.generator.get_node_innovation_number())
-        self.assertEqual(3, self.generator.get_node_innovation_number())
+        generator = InnovationNumberGeneratorSingleCore()
+        # Nodes
+        self.assertEqual(0, generator.get_node_innovation_number())
+        self.assertEqual(1, generator.get_node_innovation_number(node1, node2))
+        self.assertEqual(1, generator.get_node_innovation_number(node1, node2))
+
+        # Connections
+        self.assertEqual(0, generator.get_connection_innovation_number())
+        self.assertEqual(1, generator.get_connection_innovation_number(node1, node3))
+        self.assertEqual(1, generator.get_connection_innovation_number(node1, node3))
+
+        generator.next_generation(5)
+
+        # Nodes
+        self.assertEqual(2, generator.get_node_innovation_number(node1, node2))
+        self.assertEqual(2, generator.get_node_innovation_number(node1, node2))
+
+        # Connections
+        self.assertEqual(2, generator.get_connection_innovation_number())
+        self.assertEqual(1, generator.get_connection_innovation_number(node1, node3))
+        self.assertEqual(3, generator.get_connection_innovation_number(node2, node3))
+        self.assertEqual(3, generator.get_connection_innovation_number(node2, node3))
+
+    def test_get_node_innovation_number(self):
+        node1 = Node(1, NodeType.INPUT, step_function, 0)
+        node2 = Node("abc", NodeType.HIDDEN, step_function, 0.5)
+        node3 = Node(3, NodeType.OUTPUT, step_function, 1)
+
+        generator = InnovationNumberGeneratorSingleCore()
+        self.assertEqual(0, generator.get_node_innovation_number())
+        self.assertEqual(1, generator.get_node_innovation_number())
+        self.assertEqual(2, generator.get_node_innovation_number(node1, node2))
+        self.assertEqual(2, generator.get_node_innovation_number(node1, node2))
+        self.assertEqual(3, generator.get_node_innovation_number(node1, node3))
+        self.assertEqual(4, generator.get_node_innovation_number())
+        self.assertEqual(3, generator.get_node_innovation_number(node1, node3))
+        self.assertEqual(5, generator.get_node_innovation_number(node3, node1))
+
+        self.assertEqual(0, generator.get_connection_innovation_number())
+
+    def test_get_connection_innovation_number(self):
+        node1 = Node(1, NodeType.INPUT, step_function, 0)
+        node2 = Node("abc", NodeType.HIDDEN, step_function, 0.5)
+        node3 = Node(3, NodeType.OUTPUT, step_function, 1)
+
+        generator = InnovationNumberGeneratorSingleCore()
+        self.assertEqual(0, generator.get_connection_innovation_number())
+        self.assertEqual(1, generator.get_connection_innovation_number())
+        self.assertEqual(2, generator.get_connection_innovation_number(node1, node3))
+        self.assertEqual(2, generator.get_connection_innovation_number(node1, node3))
+        self.assertEqual(3, generator.get_connection_innovation_number())
+        self.assertEqual(4, generator.get_connection_innovation_number(node2, node3))
+        self.assertEqual(4, generator.get_connection_innovation_number(node2, node3))
+        self.assertEqual(2, generator.get_connection_innovation_number(node1, node3))
+        self.assertEqual(5, generator.get_connection_innovation_number(node3, node1))
+
+        self.assertEqual(0, generator.get_node_innovation_number())
