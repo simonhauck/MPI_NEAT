ede3804f73dfe4318e9ff164232e940543ef03f8
Simon Hauck <simonhauck@gmx.de>
2020-10-19 16:19:48 +0200

    update text visualization of genomes

--- code/src/utils/visualization/text_visualization.py
+++ code/src/utils/visualization/text_visualization.py
@@ -14,8 +14,9 @@ def print_genome(genome: Genome) -> None:
     logger.info("--- Nodes ---")
     for node in genome.nodes:
         logger.info(
-            "InnovationNumber: {}, Type: {}, Bias: {}, X-Position: {}".format(node.innovation_number, node.node_type,
-                                                                              node.bias, node.x_position))
+            "InnovationNumber: {}, Type: {}, ActivationFunction: {}, Bias: {}, X-Position: {}".format(
+                node.innovation_number, node.node_type, node.activation_function,
+                node.bias, node.x_position))
     logger.info("--- Connections ---")
     for connection in genome.connections:
         logger.info("{} -> {}, Enabled: {}, InnovationNumber: {}, Weight: {}".
