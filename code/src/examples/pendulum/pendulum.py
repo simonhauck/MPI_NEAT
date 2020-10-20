0f97218fe6b20b6cc7705bcae4797b22c00a4149
Simon Hauck <simonhauck@gmx.de>
2020-10-20 11:44:19 +0200

    cleanup pendulum

--- code/src/examples/pendulum/pendulum.py
+++ code/src/examples/pendulum/pendulum.py
@@ -1,25 +1,25 @@
+import time
 from datetime import datetime
+from typing import List
 
-import matplotlib.pyplot as plt
 import numpy as np
-import progressbar
 from loguru import logger
 
 from examples.BaseExample import BaseExample
 from examples.pendulum.pendulum_challenge import PendulumChallenge
-from neat_core.activation_function import sigmoid_activation
-from neat_core.models.agent import Agent
+from neat_core.activation_function import tanh_activation
 from neat_core.models.generation import Generation
 from neat_core.models.genome import Genome
 from neat_core.optimizer.neat_config import NeatConfig
 from neat_core.optimizer.neat_optimizer import NeatOptimizer
+from neat_core.optimizer.neat_reporter import NeatReporter
 from neural_network.basic_neural_network import BasicNeuralNetwork
 from utils.fitness_evaluation import fitness_evaluation_utils
 from utils.reporter.checkpoint_reporter import CheckPointReporter
 from utils.reporter.fitness_reporter import FitnessReporter
 from utils.reporter.species_reporter import SpeciesReporter
 from utils.reporter.time_reporter import TimeReporter
-from utils.visualization import text_visualization, reporter_visualization, genome_visualization
+from utils.visualization import text_visualization
 
 
 class PendulumOptimizer(BaseExample):
@@ -31,14 +31,7 @@ class PendulumOptimizer(BaseExample):
         self.check_point_reporter = None
         self.challenge = None
 
-        self.progressbar = None
-        self.progressbar_max = None
-        self.progressbar_widgets = [
-            'Generation: ', progressbar.Percentage(),
-            ' ', progressbar.Bar(marker='#', left='[', right=']'),
-            ' ', progressbar.Counter('Evaluated Agents: %(value)03d'),
-            ', ', progressbar.ETA()
-        ]
+        self.start_time_generation = None
 
     def evaluate(self, optimizer: NeatOptimizer, seed: int = None, **kwargs):
         time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
@@ -60,28 +53,25 @@ class PendulumOptimizer(BaseExample):
 
         # Config
         config = NeatConfig(allow_recurrent_connections=False,
-                            population_size=400,
-                            compatibility_threshold=3,
+                            population_size=1000,
+                            compatibility_threshold=4,
                             weight_mutation_type="normal",
-                            weight_mutation_normal_sigma=1.3,
-                            connection_initial_min_weight=-5,
-                            connection_initial_max_weight=5,
+                            weight_mutation_normal_sigma=0.5,
+                            connection_initial_min_weight=-1,
+                            connection_initial_max_weight=1,
                             connection_min_weight=-15,
                             connection_max_weight=15,
                             bias_mutation_type="normal",
-                            bias_mutation_normal_sigma=1.3,
+                            bias_mutation_normal_sigma=0.5,
                             bias_initial_min=-1,
                             bias_initial_max=1,
                             bias_min=-15,
                             bias_max=15,
                             compatibility_factor_disjoint_genes=1.0,
-                            compatibility_factor_matching_genes=0.5,
+                            compatibility_factor_matching_genes=4.0,
                             probability_mutate_add_connection=0.5,
                             probability_mutate_add_node=0.2,
                             compatibility_genome_size_threshold=0)
-
-        # Progressbar size
-        self.progressbar_max = config.population_size
 
         # Create random seed, if none is specified
         if seed is None:
@@ -94,7 +84,7 @@ class PendulumOptimizer(BaseExample):
         # Start evaluation
         optimizer.evaluate(amount_input_nodes=3,
                            amount_output_nodes=1,
-                           activation_function=sigmoid_activation,
+                           activation_function=tanh_activation,
                            challenge=self.challenge,
                            config=config,
                            seed=seed)
@@ -105,43 +95,26 @@ class PendulumOptimizer(BaseExample):
     def on_generation_evaluation_start(self, generation: Generation) -> None:
         logger.info(
             "Starting evaluation of generation {} with {} agents".format(generation.number, len(generation.agents)))
-        self.progressbar = progressbar.ProgressBar(widgets=self.progressbar_widgets, max_value=self.progressbar_max)
-        self.progressbar.start()
+        self.start_time_generation = time.time()
 
-    def on_agent_evaluation_end(self, i: int, agent: Agent) -> None:
-        self.progressbar.update(i + 1)
-
-    def on_generation_evaluation_end(self, generation: Generation) -> None:
-        self.progressbar.finish()
+    def on_generation_evaluation_end(self, generation: Generation, reporters: List[NeatReporter]) -> None:
+        required_time = round(time.time() - self.start_time_generation, 4)
+        logger.info("Finished evaluation of generation {}, Required Time: {}".format(generation.number, required_time))
 
         best_agent = fitness_evaluation_utils.get_best_agent(generation.agents)
-
-        logger.info("Finished evaluation of generation {}".format(generation.number))
         logger.info("Best Fitness in Agent {}: {}, AdditionalInfo: {}".format(best_agent.id, best_agent.fitness,
                                                                               best_agent.additional_info))
         logger.info("Amount species: {}".format(len(generation.species_list)))
 
-    def on_finish(self, generation: Generation) -> None:
+    def on_finish(self, generation: Generation, reporters: List[NeatReporter]) -> None:
         logger.info("OnFinish called with generation {}".format(generation.number))
 
         # Get the best agent
         agent = fitness_evaluation_utils.get_best_agent(generation.agents)
+        logger.info("Best Agent - Fitness:  {}, AdditionalInfo: {}".format(agent.fitness, agent.additional_info))
 
         # Print genome
         text_visualization.print_agent(agent)
-
-        # Plot fitness values
-        reporter_visualization.plot_fitness_reporter(self.fitness_reporter.data, plot=True)
-
-        # Plot species sizes
-        reporter_visualization.plot_species_reporter(self.species_reporter.data, plot=True)
-
-        # Plot required time values
-        reporter_visualization.plot_time_reporter(self.time_reporter.data, plot=True)
-
-        # Plot genome
-        genome_visualization.draw_genome_graph(agent.genome, draw_labels=False)
-        plt.show()
 
     def finish_evaluation(self, generation: Generation) -> bool:
         best_agent = fitness_evaluation_utils.get_best_agent(generation.agents)
--- code/src/examples/pendulum/pendulum_challenge.py
+++ code/src/examples/pendulum/pendulum_challenge.py
@@ -17,7 +17,7 @@ class PendulumChallenge(Challenge):
         self.env = gym.make("Pendulum-v0")
 
     def before_evaluation(self, **kwargs) -> None:
-        # self.env.seed(1)
+        self.env.seed(1)
         self.observation = self.env.reset()
 
     def evaluate(self, neural_network: NeuralNetworkInterface, **kwargs) -> (float, Dict[str, object]):
@@ -25,7 +25,7 @@ class PendulumChallenge(Challenge):
 
         solved_rounds = []
         fitness_values = []
-        amount_runs = 10
+        amount_runs = 1
 
         for _ in range(amount_runs):
 
@@ -35,8 +35,8 @@ class PendulumChallenge(Challenge):
             for _ in range(max_steps):
                 modified_obs = [self.observation[0], self.observation[1], self.observation[2] / 8]
                 action = neural_network.activate(modified_obs)
-                # Parse the result from range 0, 1 to range -2, 2
-                force = -2 + action[0] * 4
+                # Parse the result from range -1, 1 to range -2, 2
+                force = action[0] * 2
 
                 self.observation, reward, _, _ = self.env.step([force])
 
@@ -48,8 +48,8 @@ class PendulumChallenge(Challenge):
             solved_rounds.append(fitness >= -200)
             fitness_values.append(fitness)
 
-        return -(np.sum(fitness_values) ** 2), {"solved": all(solved_rounds), "fitness_values: ": fitness_values,
-                                                "solved_rounds": solved_rounds}
+        return (np.sum(fitness_values)), {"solved": all(solved_rounds), "fitness_values: ": fitness_values,
+                                          "solved_rounds": solved_rounds}
 
     def clean_up(self, **kwargs):
         self.env.close()
